01a7e89c78c26c5ce795861f42346a7f24e889f4
Vesna Zaric <4vesnazaric@gmail.com>
2023-06-04 17:09:44 -0700

    commented out all movements

--- client.js
+++ client.js
@@ -1,37 +1,39 @@
 const net = require("net")
 
-//establishes a connection with the game server
-const connect = function () {
-  const conn = net.createConnection({   //You used Node's net library (specifically, the createConnection function) to create an object named conn 
-    host: '192.168.1.13', // IP address here,
-    port: 50541      // PORT number here,
-  });
-// The conn object is full of useful methods and properties that can now be used to interact with the server! You will use the conn object to handle messages from the server.
+let intervalId;
 
+const connect = function () {
+  const conn = net.createConnection({  
+    host: '192.168.1.13', 
+    port: 50541,
+  });
   // next, interpret incoming data as text
   conn.setEncoding("utf8");
-
-  //conn.on("data", (data) => {
-  //  console.log("Received data ", data);
-  //});
 
 
   //use the .on method on our conn object to register a "connect" handler (a callback function). In the callback, print a message
   conn.on("connect", () => {
     console.log("Connected to game server");
     conn.write("Name: ABC"); //Send the string "Name: ___" to the server, upon connection
-    setTimeout(() => {
-    conn.write("Move: up");
-    }, 2000); 
-    setTimeout(() => {
-    conn.write("Move: down");
-    }, 1000);
-    setTimeout(() => {    
-      
-    conn.write("Move: left");
-    conn.write("Move: right");
-    }, 3000);
 
+     
+  //    intervalId = setInterval(() => {   
+  //  // conn.write("Move: up");
+  //  // }, 50); 
+
+  //   setInterval(() => {  // smooth movement accross the board
+  //   conn.write("Move: down");
+  //   }, 50);
+     
+  
+  //   setInterval(() => { 
+  //   conn.write("Move: left");
+  //   }, 50);
+
+  //   setInterval(() => {
+  //   conn.write("Move: right");
+  //   }, 50);
+  
   });
   conn.on("data", (data) => {
     console.log("Received data: ", data);
@@ -41,5 +43,9 @@ const connect = function () {
 
   return conn;
 };
+// setTimeout(() => {
+//   clearInterval(intervalId);
+// }, 5000);
+
 
 module.exports = {connect}
