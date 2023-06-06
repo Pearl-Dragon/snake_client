2df3cb7d0cbf74846e5f782e1d8e49ba207076ea
Vesna Zaric <4vesnazaric@gmail.com>
2023-06-06 06:53:24 -0700

    re-done code from connection module

--- play.js
+++ play.js
@@ -1,10 +1,21 @@
-const { connect } = require("./client");
-const setupInput = require("./input");
+
+const net = require("net");
+const connect = require("./client")
+// establishes a connection with the game server
+// const connect = function () {
+//   const conn = net.createConnection({
+//     host: 'localhost',// IP address here,
+//     port: 50541,// PORT number here,
+//   });
+
+  // interpret incoming data as text
+  conn.setEncoding("utf8");
+conn.on("data", (data) => {
+  console.log("Recieved data:", data);
+});
+
+  return conn;
+
 
 console.log("Connecting ...");
-
-// Call the connect function to establish the connection
 connect();
-
-// Call the setupInput function to set up user input handling
-setupInput();
