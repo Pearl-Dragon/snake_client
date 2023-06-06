a74ee7f83bc754e967e7c615f8229f0f5504f136
Vesna Zaric <4vesnazaric@gmail.com>
2023-06-06 07:04:08 -0700

    adding snake initials for the second time

--- client.js
+++ client.js
@@ -1,51 +1,28 @@
 const net = require("net")
 
-let intervalId;
-
-const connect = function () {
-  const conn = net.createConnection({  
-    host: '192.168.1.13', 
-    port: 50541,
+const connect = () => {
+  const conn = net.createConnection({
+    host: 'localhost',// IP address here,
+    port: 50541,// PORT number here,
   });
-  // next, interpret incoming data as text
+
+  // interpret incoming data as text
   conn.setEncoding("utf8");
 
-
-  //use the .on method on our conn object to register a "connect" handler (a callback function). In the callback, print a message
-  conn.on("connect", () => {
-    console.log("Connected to game server");
-    conn.write("Name: ABC"); //Send the string "Name: ___" to the server, upon connection
-
-     
-  //    intervalId = setInterval(() => {   
-  //  // conn.write("Move: up");
-  //  // }, 50); 
-
-  //   setInterval(() => {  // smooth movement accross the board
-  //   conn.write("Move: down");
-  //   }, 50);
-     
-  
-  //   setInterval(() => { 
-  //   conn.write("Move: left");
-  //   }, 50);
-
-  //   setInterval(() => {
-  //   conn.write("Move: right");
-  //   }, 50);
-  
-  });
+  // Handle incoming data
   conn.on("data", (data) => {
-    console.log("Received data: ", data);
+    console.log("Received data:", data);
+    // You can perform any additional processing or actions with the data here
   });
-
-  console.log("Connecting ...");
-
+// Handle successful connection
+conn.on('connect', () => {
+  console.log('Successfully connected to game server');
+  // Send the name message
+  conn.write('Name: XYZ'); // Replace XYZ with your initials
+});
   return conn;
 };
-// setTimeout(() => {
-//   clearInterval(intervalId);
-// }, 5000);
 
+console.log("Connecting ...");
 
-module.exports = {connect}
+module.exports = connect;
